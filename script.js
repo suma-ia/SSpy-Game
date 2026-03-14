e1fe312f539105d1241f813c50b119d235cee890
suma-ia <sumaiiiia.05@gmail.com>
2026-03-15 02:08:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,116 @@
+let players = 0;
+let spies = 0;
+let roles = [];
+let currentPlayer = 0;
+
+let time = 180;
+let timer = null;
+
+function startGame(){
+
+players = parseInt(document.getElementById("players").value);
+spies = parseInt(document.getElementById("spies").value);
+
+if(players <= 0 || spies < 0 || spies >= players){
+alert("Invalid numbers");
+return;
+}
+
+roles = [];
+
+for(let i=0;i<spies;i++){
+roles.push("Spy");
+}
+
+for(let i=0;i<players-spies;i++){
+roles.push("Citizen");
+}
+
+roles.sort(()=>Math.random()-0.5);
+
+document.getElementById("setup").style.display="none";
+document.getElementById("game").style.display="block";
+
+currentPlayer = 0;
+
+document.getElementById("playerTurn").innerText =
+"Player " + (currentPlayer+1);
+
+}
+
+function showRole(){
+document.getElementById("role").innerText = roles[currentPlayer];
+}
+
+function nextPlayer(){
+
+document.getElementById("role").innerText="";
+
+currentPlayer++;
+
+if(currentPlayer >= players){
+
+document.getElementById("playerTurn").innerText =
+"All players have their roles!";
+
+document.getElementById("revealBtn").style.display="none";
+document.getElementById("nextBtn").style.display="none";
+
+document.getElementById("timer").style.display="block";
+document.getElementById("revealSpiesBtn").style.display="block";
+
+startTimer();
+
+return;
+}
+
+document.getElementById("playerTurn").innerText =
+"Player " + (currentPlayer+1);
+
+}
+
+function startTimer(){
+
+if(timer !== null){
+clearInterval(timer);
+}
+
+time = 180;
+
+timer = setInterval(function(){
+
+let minutes = Math.floor(time/60);
+let seconds = time % 60;
+
+if(seconds < 10){
+seconds = "0" + seconds;
+}
+
+document.getElementById("timer").innerText =
+minutes + ":" + seconds;
+
+time--;
+
+if(time < 0){
+clearInterval(timer);
+alert("Time is over!");
+}
+
+},1000);
+
+}
+
+function revealSpies(){
+
+let spyPlayers = [];
+
+for(let i=0;i<roles.length;i++){
+if(roles[i] === "Spy"){
+spyPlayers.push("Player " + (i+1));
+}
+}
+
+document.getElementById("spyList").innerText =
+"Spy is: " + spyPlayers.join(", ");
+
+}
